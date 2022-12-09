827da6104b865ef0f13399740b088a1623059734
Nkululeko Jonas <nkululeko.jonas@icloud.com>
2022-12-10 09:02:55 +1000

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Nkululeko Jonas on 10/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
