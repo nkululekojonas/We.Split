ac22348c7f08e887a8482b3e9bea3d87679da965
nkululekojonas <nkululeko.jonas@icloud.com>
2022-12-11 14:05:12 +1000

    Add control and functionality

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var tipPercentage = 0
+    @State private var numberOfPeople = 0
+    @FocusState private var amountIsFocus: Bool
+    
+    private let tipPercentages: [Int] = [0, 10, 15, 20, 25]
+    private var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return amountPerPerson
+
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "AUD"))
+                        .keyboardType( .decimalPad)
+                        .focused($amountIsFocus)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<99) {
+                            Text("\($0)")
+                        }
+                    }
+                }
+                
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }.pickerStyle(.segmented)
+                } header: {
+                    Text("How much would you like to tip:")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "AUD"))
+                }
+            }
+            .navigationTitle("We.Split")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        amountIsFocus = false
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
